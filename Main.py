6cd06f6494134b44642242487bf6a036427ff775
JacobSalam <jacob_salam@hotmail.com>
2018-05-17 13:14:30 +0200

    Commented version of tf-idf

--- Main.py
+++ Main.py
@@ -9,6 +9,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 lines = []
 lines2 =[]
 count = 0
+
+# Opens text file and puts all the lines in a lines array
 with open("text.txt",encoding = 'latin-1') as f:
     for line in f:
         lines.append(line)
@@ -16,6 +18,8 @@ with open("text.txt",encoding = 'latin-1') as f:
         
 print(count)
 b = "!@#$.,?-/_%^&*()+=\":;"
+
+# Removes all the characters that are not needed
 for line in lines:
     for char in b:
         line = line.replace(char,"")
@@ -24,10 +28,12 @@ for line in lines:
 lines = lines2
         
         
-
-query = ["murder","death","died","killed","murdered","killed","die","murderer","shot","poisoned","body"]
+# The query that is used for tf-idf, the query is a simple one that
+# uses typicle words for murders
+query = ["murder","death","died","killed","murdered","killed","die","murderer"]
 queryCount = []
 
+# Query count is used for tf to hold the count the query words in a certain document
 for a in range(len(query)):
     a = numpy.zeros(389)
     queryCount.append(a)
@@ -37,16 +43,9 @@ doculist = []
 d2 =[]
 count =0
 
-'''
-for j in range(100):
-    document = ''
-    for i in range(39):
-        line = lines[i+count]
-        document += str(line)
-    doculist.append(document)
-    count = count+39
-'''
 
+# splits the text file into multiple documents, in this case:
+# each document is 15 lines, no recursion of sentences (was not usefull)
 for j in range(259):
     document = ''
     doc2 = ""
@@ -58,6 +57,7 @@ for j in range(259):
     d2.append(doc2)
     count += 15
 
+# Collects the count of words
 def wordCount(a,b):
     queryCount[a][b] += 1
     
@@ -65,6 +65,8 @@ def wordCount(a,b):
 check = False
 qCount =0
 docCounter =0
+
+# tf : counts the words of the query
 for doc in d2:
     number = 0
     result = doc.split()
@@ -74,32 +76,16 @@ for doc in d2:
             w = w.lower()
             if word == w:
                 wordCount(qCount, docCounter)
-                if word == "died" and docCounter > 500:
-                   check = True
             qCount +=1
-  
-        if check == True:
-            #print(queryCount[0][779-13])
-            check = False
-            
-        
-        
-    #if number > 5:
-        #print(number)
-        #print(doc)
-        #print(docCounter)
-        #number = 0
-        
     docCounter +=1
 
 
     
         
 
-vectorizer = CountVectorizer()
 
-X = vectorizer.fit_transform(doculist)
 
+#idf function using sklearn
 vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(doculist)
 idf = vectorizer.idf_
@@ -110,21 +96,28 @@ feature_names = vectorizer.get_feature_names()
 count = 0
 score = 0
 docWeight = []
+# docWeight holds the weight of each document, the higher the weight the more "usefull"
+# the document (not all info is found in these "usefull" documents), sometimes murder or
+# death is not always mentioned, but usually in the area of these document a murder has
+# happened
+
 for a in range(len(doculist)):
     a = numpy.zeros(1)
     docWeight.append(a)
 
+# Calculates the tf-idf scores for each document
 for i in range(len(doculist)):
     for j in range(len(query)):
         score = score + (idf[vectorizer.vocabulary_.get(query[j])] * queryCount[j][i])
     docWeight[i] = score
+    #Threshold placed by me (You can make it whatever you want)
     if score > 30:
-        print(i)
-        print (score)
-        print(doculist[i])
+        print(i) # prints the document number 
+        print (docWeight[i]) # Score of document i
+        print(doculist[i]) # put i in the doculist array, returns the text found
     score = 0;
 
-
+'''
 print(vectorizer.vocabulary_.get('murderess'))
 print(idf[vectorizer.vocabulary_.get('murderess')])
 print(feature_names[vectorizer.vocabulary_.get('murderess')])
@@ -133,7 +126,7 @@ print(feature_names[vectorizer.vocabulary_.get('murderess')])
 text_file = open("Output.txt", "wb")
 text_file.write(str(dict(zip(vectorizer.get_feature_names(),idf) )).encode('utf-8').strip())
 text_file.close()
-
+'''
 
 
 
